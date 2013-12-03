027a23cd9490e35dfaf63f3fcba6565d7fd3d6f4
Joseph Utecht <thenmal@gmail.com>
2013-12-03 16:12:29 -0600

    added license

--- imgur-upload.py
+++ imgur-upload.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+#   Copyright 2013 Joseph Utecht
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 from gimpfu import *
 import os
 import json
@@ -51,7 +67,7 @@ register(
     "Upload the current image to imgur",
     "Save the current layer into a PNG file, and then upload to imgur.",
     "JRU",
-    "Open source (BSD 3-clause license)",
+    "Open source (GPLv3)",
     "2013",
     "<Image>/Filters/Imgur/Upload Current Layer",
     "*",
